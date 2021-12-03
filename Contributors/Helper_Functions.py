935c231fa8fa6f27e294cda925dfb750d32546df
jgeorge313 <32001894+jgeorge313@users.noreply.github.com>
2021-12-03 13:23:09 -0500

    Create Helper_Functions.py

---
+++ Contributors/Helper_Functions.py
@@ -0,0 +1,13 @@
+"""
+Helper Functions.py
+
+
+A place to upload helper functions that will be used in the 
+DS GA 1001 Capstone Project
+
+Changelog:
+    v0: Joby initialized the file
+    v1: x person inputted y functions
+    v2:
+    ...
+"""
