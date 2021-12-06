1dd829cc2ddde96e22eb240a689a1df67ac62682
alexxvives <39196942+alexxvives@users.noreply.github.com>
2021-12-06 16:48:42 -0500

    Update Helper_Functions.py

--- Contributors/Helper_Functions.py
+++ Contributors/Helper_Functions.py
@@ -119,7 +119,67 @@ def hypothesis_data2(df, control_column1, control_column2, filter_column, return
     
     return dict_
 
-def hypothesis_data3(data_frame, cutoff_array):
+def hypothesis_data3(df, bucket_list):
+    
+    #bucket list is an array of buckets to group respondents by
+    #returns dictionary with experience bucket top value key and 2 arrays of salaries [female, male] as value
+    #relies on Jonah's original control for experience
+    
+    final_dict = dict()
+    filtered_df=df.dropna(subset = ['gender'])
+    filtered_df.sort_values('yearsofexperience')
+    filtered_male = filtered_df[filtered_df['gender'] == 'Male']
+    filtered_female = filtered_df[filtered_df['gender'] == 'Female']
+    filtered_male = filtered_male.sort_values('yearsofexperience')
+    filtered_female = filtered_female.sort_values('yearsofexperience')
+    
+    male_cutoffs = control_for_experience(filtered_male, bucket_list)
+    female_cutoffs = control_for_experience(filtered_female, bucket_list)
+    
+    i=0
+    while i < len(bucket_list):
+        
+        male_array = list(filtered_male['totalyearlycompensation'])[:male_cutoffs[i]]
+        female_array = list(filtered_female['totalyearlycompensation'])[:female_cutoffs[i]]
+        final_dict[bucket_list[i]]= [female_array, male_array]
+        i+=1
+        
+    return (final_dict)
+
+def hypothesis_data4(df,bucketlist):
+    
+    bucketlist = [0] + bucketlist + [100]
+    filtered_df=df.dropna(subset = ['Education'])
+    filtered_HS = filtered_df[filtered_df['Education'] == "Highschool"]
+    filtered_HS = filtered_HS.sort_values('yearsofexperience')
+    filtered_bachelor = filtered_df[filtered_df['Education'] == "Bachelor's Degree"]
+    filtered_bachelor = filtered_bachelor.sort_values('yearsofexperience')
+    filtered_master = filtered_df[filtered_df['Education'] == "Master's Degree"]
+    filtered_master = filtered_master.sort_values('yearsofexperience')
+    filtered_phd = filtered_df[filtered_df['Education'] == "PhD"]
+    filtered_phd = filtered_phd.sort_values('yearsofexperience')
+    
+    hsarray = []
+    collegearray = []
+    masterarray = []
+    phdarray = []
+    i=0
+    while i < len(bucketlist)-1:
+            hsarray.append(list(filtered_HS['totalyearlycompensation'][(filtered_HS['yearsofexperience']>=bucketlist[i]) & (filtered_HS['yearsofexperience']<bucketlist[i+1])]))
+            collegearray.append(list(filtered_bachelor['totalyearlycompensation'][(filtered_bachelor['yearsofexperience']>=bucketlist[i]) & (filtered_bachelor['yearsofexperience']<bucketlist[i+1])]))
+            masterarray.append(list(filtered_master['totalyearlycompensation'][(filtered_master['yearsofexperience']>=bucketlist[i]) & (filtered_master['yearsofexperience']<bucketlist[i+1])]))
+            phdarray.append(list(filtered_phd['totalyearlycompensation'][(filtered_phd['yearsofexperience']>=bucketlist[i]) & (filtered_phd['yearsofexperience']<bucketlist[i+1])]))
+            i=i+1
+         
+    j = 0
+    dict_ = {}    
+    while j < len(hsarray):
+        dict_[str(bucketlist[j]) + '-' + str(bucketlist[j+1])] = [hsarray[j],collegearray[j],masterarray[j],phdarray[j]]
+        j += 1
+        
+    return dict_
+
+def hypothesis_data5(data_frame, cutoff_array):
     """
     takes in a dataframe and creates a region column for the data, if it does not exist continue,
     input an array to look at experience cutoff groups and then return a dictionary that 
@@ -160,66 +220,6 @@ def hypothesis_data3(data_frame, cutoff_array):
     final_dict = dict(zip(final_keys, big_list))
     
     return(final_dict)
-
-def hypothesis_data4(df,bucketlist):
-    
-    bucketlist = [0] + bucketlist + [100]
-    filtered_df=df.dropna(subset = ['Education'])
-    filtered_HS = filtered_df[filtered_df['Education'] == "Highschool"]
-    filtered_HS = filtered_HS.sort_values('yearsofexperience')
-    filtered_bachelor = filtered_df[filtered_df['Education'] == "Bachelor's Degree"]
-    filtered_bachelor = filtered_bachelor.sort_values('yearsofexperience')
-    filtered_master = filtered_df[filtered_df['Education'] == "Master's Degree"]
-    filtered_master = filtered_master.sort_values('yearsofexperience')
-    filtered_phd = filtered_df[filtered_df['Education'] == "PhD"]
-    filtered_phd = filtered_phd.sort_values('yearsofexperience')
-    
-    hsarray = []
-    collegearray = []
-    masterarray = []
-    phdarray = []
-    i=0
-    while i < len(bucketlist)-1:
-            hsarray.append(list(filtered_HS['totalyearlycompensation'][(filtered_HS['yearsofexperience']>=bucketlist[i]) & (filtered_HS['yearsofexperience']<bucketlist[i+1])]))
-            collegearray.append(list(filtered_bachelor['totalyearlycompensation'][(filtered_bachelor['yearsofexperience']>=bucketlist[i]) & (filtered_bachelor['yearsofexperience']<bucketlist[i+1])]))
-            masterarray.append(list(filtered_master['totalyearlycompensation'][(filtered_master['yearsofexperience']>=bucketlist[i]) & (filtered_master['yearsofexperience']<bucketlist[i+1])]))
-            phdarray.append(list(filtered_phd['totalyearlycompensation'][(filtered_phd['yearsofexperience']>=bucketlist[i]) & (filtered_phd['yearsofexperience']<bucketlist[i+1])]))
-            i=i+1
-         
-    j = 0
-    dict_ = {}    
-    while j < len(hsarray):
-        dict_[str(bucketlist[j]) + '-' + str(bucketlist[j+1])] = [hsarray[j],collegearray[j],masterarray[j],phdarray[j]]
-        j += 1
-        
-    return dict_
-
-def hypothesis_data5(df, bucket_list):
-    
-    #bucket list is an array of buckets to group respondents by
-    #returns dictionary with experience bucket top value key and 2 arrays of salaries [female, male] as value
-    #relies on Jonah's original control for experience
-    
-    final_dict = dict()
-    filtered_df=df.dropna(subset = ['gender'])
-    filtered_df.sort_values('yearsofexperience')
-    filtered_male = filtered_df[filtered_df['gender'] == 'Male']
-    filtered_female = filtered_df[filtered_df['gender'] == 'Female']
-    filtered_male = filtered_male.sort_values('yearsofexperience')
-    filtered_female = filtered_female.sort_values('yearsofexperience')
-    
-    male_cutoffs = control_for_experience(filtered_male, bucket_list)
-    female_cutoffs = control_for_experience(filtered_female, bucket_list)
-    
-    i=0
-    while i < len(bucket_list):
-        
-        male_array = list(filtered_male['totalyearlycompensation'])[:male_cutoffs[i]]
-        female_array = list(filtered_female['totalyearlycompensation'])[:female_cutoffs[i]]
-        final_dict[bucket_list[i]]= [female_array, male_array]
-        i+=1
-        
-    return (final_dict)
 
 
 #Takes a dataframe, matches 161 company names for their industry, returns the altered dataframe
