c80e248b684e5ed5f574c07c919d53830d0d5f86
Jbug1 <jonahpocz@gmail.com>
2021-12-18 16:10:25 -0500

    Update Helper_Functions.py

--- Contributors/Helper_Functions.py
+++ Contributors/Helper_Functions.py
@@ -157,32 +157,23 @@ def hypothesis_data2(df, control_column1, control_column2, filter_column, return
         dict_[str(cutoffs[i]) + '-' + str(cutoffs[i+1])] = [faang, nonfaang_tech]
     return dict_
 
-def hypothesis_data3(df, bucket_list):
-    
-    #bucket list is an array of buckets to group respondents by
-    #returns dictionary with experience bucket top value key and 2 arrays of salaries [female, male] as value
-    #relies on Jonah's original control for experience
+def hypothesis_data3(df, buckets):
     
     final_dict = dict()
-    filtered_df=df.dropna(subset = ['gender'])
-    filtered_df.sort_values('yearsofexperience')
-    filtered_male = filtered_df[filtered_df['gender'] == 'Male']
-    filtered_female = filtered_df[filtered_df['gender'] == 'Female']
-    filtered_male = filtered_male.sort_values('yearsofexperience')
-    filtered_female = filtered_female.sort_values('yearsofexperience')
-    
-    male_cutoffs = control_for_experience(filtered_male, bucket_list)
-    female_cutoffs = control_for_experience(filtered_female, bucket_list)
-    
-    i=0
-    while i < len(bucket_list):
+    buckets = [0]+buckets
+    i=1
+    while i<len(buckets):
         
-        male_array = list(filtered_male['totalyearlycompensation'])[:male_cutoffs[i]]
-        female_array = list(filtered_female['totalyearlycompensation'])[:female_cutoffs[i]]
-        final_dict[bucket_list[i]]= [female_array, male_array]
+        exp_df= df[df['yearsofexperience'].between(buckets[i-1],buckets[i])]
+        exp_df.reset_index()
+        dud = exp_df[exp_df['gender']=='Male']
+        fem = exp_df[exp_df['gender']=='Female']
+        
+        key= str(buckets[i-1])+'-'+str(buckets[i])
+        final_dict[key]= [list(fem['totalyearlycompensation']),list(dud['totalyearlycompensation'])]
         i+=1
-        
-    return (final_dict)
+    
+    return(final_dict) 
 
 def hypothesis_data4(df,bucketlist):
     
